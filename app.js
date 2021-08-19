cf7cafcab5635bae091c9796d21fa336628a5640
MariaKnjazyk <mashaknjazyk@gmail.com>
2021-08-19 22:28:01 +0300

    Homework 2; const pathStatic

--- app.js
+++ app.js
@@ -13,10 +13,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.use(express.static(path.join(__dirname, 'static')));
+const pathStatic = path.join(__dirname, 'static');
+
+app.use(express.static(pathStatic));
 app.set('view engine', '.hbs');
 app.engine('.hbs', expressHbs({defaultLayout: false}));
-app.set('views', path.join(__dirname, 'static'));
+app.set('views', pathStatic);
 
 
 app.get('/ping', (req, res) => {
